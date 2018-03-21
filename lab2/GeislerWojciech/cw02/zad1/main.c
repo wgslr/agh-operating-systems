c0ebb76b9c0de3718adc0394f470b6be7b2a4850
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-03-21 01:31:56 +0100

    Fix flags_to_char

--- lab2/GeislerWojciech/cw02/zad1/main.c
+++ lab2/GeislerWojciech/cw02/zad1/main.c
@@ -9,6 +9,8 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <time.h>
+#include <assert.h>
+#include <errno.h>
 
 
 typedef struct File {
@@ -40,19 +42,24 @@ void print_timing(const timestamp start, const timestamp end, const char *descri
 // Returned array should be free-ed
 char *flags_to_char(const int flags) {
     char *char_flags = calloc(3, sizeof(char));
-    if(flags & O_APPEND) {
+    if((flags & O_RDWR) == O_RDWR) {
+        if((flags & O_TRUNC) == O_TRUNC && (flags & O_CREAT) == O_TRUNC){
+            char_flags[0] = 'w';
+        } else if((flags & O_APPEND) == O_APPEND) {
+            char_flags[0] = 'a';
+        } else {
+            char_flags[0] = 'r';
+        }
+        char_flags[1] = '+';
+        char_flags[2] = '\0';
+    } else if((flags & O_APPEND) == O_APPEND) {
         char_flags[0] = 'a';
         char_flags[1] = '\0';
-    }
-    if(flags & O_RDWR) {
-        char_flags[0] = 'r';
-        char_flags[1] = 'w';
-        char_flags[2] = '\0';
-    } else if(flags & O_WRONLY) {
+    } else if((flags & O_WRONLY) == O_WRONLY) {
         char_flags[0] = 'w';
         char_flags[1] = '\0';
     } else {
-        // O_RDONLY == 0
+        // Checked last as O_RDONLY == 0
         char_flags[0] = 'r';
         char_flags[1] = '\0';
     }
@@ -97,7 +104,7 @@ void file_close(File **file_ptr) {
 
 // Returns number of read bytes
 // file is either (int*) or (FILE*)
-unsigned file_read(File *file, unsigned size, void *buf) {
+unsigned file_read(const File *file, unsigned size, void *buf) {
     if(file->syscall) {
         return (unsigned) read(file->fd, buf, size);
     } else {
@@ -111,9 +118,19 @@ int file_write(File *file, unsigned size, const void *content) {
     if(file->syscall) {
         return (int) write(file->fd, content, size);
     } else {
-        return (int) fwrite(content, size, 1, file->handle);
+        return (int) (size * fwrite(content, size, 1, file->handle));
     }
 }
+
+
+void file_seek(const File *file, const off_t offset, const int whence) {
+    if(file->syscall) {
+        lseek(file->fd, offset, whence);
+    } else {
+        fseek(file->handle, offset, whence);
+    }
+}
+
 
 void copy(const char *path_from, const char *path_to,
           const unsigned records, const unsigned record_size, bool syscalls) {
@@ -146,19 +163,89 @@ void copy(const char *path_from, const char *path_to,
 
 }
 
+
 void generate(const char *path, const unsigned records, const unsigned record_size, bool syscalls) {
     copy("/dev/urandom", path, records, record_size, syscalls);
 }
 
-void sort(const char *path, const unsigned records, const unsigned record_size, bool syscalls) {
-    unsigned char *buf;
+void sort(const char *path, const int records, const int record_size, bool syscalls) {
+    unsigned char *buffer = malloc(record_size);
+    unsigned char *current = malloc(record_size);
+    File *file = file_open(path, O_RDWR, syscalls);
+
+    if(file == NULL) {
+        fprintf("Error opening file %s", path);
+        exit(1);
+    }
+
+    // begin from second record
+    file_seek(file, record_size, SEEK_SET);
+    int pos; // tracks position in file in record_size units
+    for(int i = pos = 1; i < records; ++i) {
+        if(!file_read(file, record_size, current)) {
+            break;
+        }
+        ++pos;
+
+//        // move to beginning of current record
+        file_seek(file, -(int) record_size, SEEK_CUR);
+        --pos;
+
+        while(pos > 0) { // @fixme
+            file_seek(file, -(int) record_size, SEEK_CUR);
+            --pos;
+            int bytes_read = file_read(file, record_size, buffer);
+            ++pos;
+
+            if(bytes_read < record_size) {
+                fprintf(stderr, "Error reading: %s\n", strerror(errno));
+            }
+
+            if(current[0] < buffer[0]) {
+                fprintf(stderr, "%d is smaller than %d\n", i, pos - 1);
+                // move record one position right
+                fprintf(stderr, "Writing at %u\n", ftell(file->handle));
+                int written = file_write(file, record_size, buffer);
+                assert(written == record_size);
+                ++pos;
+                // move to next record left
+                file_seek(file, -2 * (int) record_size, SEEK_CUR);
+                --pos;
+                --pos;
+            } else {
+                break;
+            }
+        }
+//        fprintf(stderr, "Move %d to %d\n", i, pos);
+        int written = file_write(file, record_size, current);
+        fprintf(stderr, "Written %d to %d\n", written, pos);
+        ++pos;
+        // move to next record
+        file_seek(file, (i - pos + 1) * record_size, syscalls);
+        pos += i - pos + 1;
+    }
+
+    file_close(&file);
+    free(buffer);
+    free(current);
 }
+
+//void write_at(const File *file, const unsigned record_size, const unsigned pos) {
+//
+//}
 
 // offset - number of arguments before size parameters
 void parse_params(int offset, char *argv[], int *records, int *record_size, bool *syscalls) {
     *records = atoi(argv[offset + 1]);
     *record_size = atoi(argv[offset + 2]);
-    *syscalls = strcmp(argv[offset + 3], "sys") == 0;
+    if(strcmp(argv[offset + 3], "sys") == 0) {
+        *syscalls = true;
+    } else if(strcmp(argv[offset + 3], "lib") == 0) {
+        *syscalls = false;
+    } else {
+        fprintf(stderr, "Incorrect argument \"%s\"\n", argv[offset + 3]);
+        exit(1);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -191,6 +278,13 @@ int main(int argc, char *argv[]) {
     } else if(strcmp(argv[1], "sort") == 0) {
         const char *path = argv[2];
         parse_params(2, argv, &records, &record_size, &syscalls);
+
+        timestamp start = get_timestamp();
+        sort(path, records, record_size, syscalls);
+        timestamp end = get_timestamp();
+
+        sprintf(description, "sort; %d; %d; %d;", records, record_size, syscalls);
+        print_timing(start, end, description);
     } else {
         fprintf(stderr, "Unidentified argument \'%s\"\n", argv[1]);
     }
