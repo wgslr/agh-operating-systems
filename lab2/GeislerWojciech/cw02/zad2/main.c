9aec9cace8c79a2702c5625a4b10cd5a505f22f0
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-03-21 07:47:12 +0100

    Fix time parsing

--- lab2/GeislerWojciech/cw02/zad2/main.c
+++ lab2/GeislerWojciech/cw02/zad2/main.c
@@ -20,17 +20,24 @@ typedef struct dirent dirent;
 
 const char *PERMISSIONS = "rwxrwxrwx";
 
-time_t target_time;
-char comparator;
+time_t target_time = 0;
+char comparator = '\0';
 
 bool is_absolute(const char *path) {
     return path[0] == '/';
 }
 
-time_t str_to_time(const char *str) {
-    struct tm time;
+// Assummes foramt yyyy-mm-dd
+time_t str_to_time(char *str) {
+    str[4] = '\0',
+    str[7] = '\0';
+    struct tm time = {0};
 
-    strptime(str, "%Y-%m-%d", &time);
+    time.tm_year = atoi(&str[0]) - 1900;
+    time.tm_mon = atoi(&str[5]) - 1;
+    time.tm_mday = atoi(&str[8]);
+    time.tm_hour = 12;
+
     return mktime(&time);
 }
 
@@ -91,7 +98,6 @@ char *absolute(const char *path) {
 
 
 void print_file(const char *path, const struct stat *stats) {
-//    fprintf(stderr, "Path: %s\n", path);
     char *name = absolute(path);
     long size = stats->st_size;
     struct timespec mtime = stats->st_mtim;
@@ -108,16 +114,27 @@ void print_file(const char *path, const struct stat *stats) {
 
 
 bool is_time_allowed(const struct stat *stats) {
+
+    fprintf(stderr, "file secs: %ld, target: %ld\n", stats->st_mtim.tv_sec, target_time);
     time_t file_days = stats->st_mtim.tv_sec / (3600 * 24);
     time_t target_days = target_time / (3600 * 24);
-    long diff = file_days - target_days;
-    return (comparator == '<' && diff < 0)
-           || (comparator == '=' && diff == 0)
-           || (comparator == '>' && diff > 0);
+    long int diff = file_days - target_days;
+
+
+    bool result = (comparator == '<' && diff < 0)
+                  || (comparator == '=' && diff == 0)
+                  || (comparator == '>' && diff > 0);
+    if(!result) {
+        fprintf(stderr, "Skipping file %ld. File is %ld where looking for %c%ld\n", stats->st_size, file_days,
+                comparator, target_days);
+    }
+    return result;
 }
 
 
 void display_dir(const char *path) {
+    fprintf(stderr, "Display dir %s\n", path);
+
     DIR *dir = opendir(path);
     dirent *entry;
     struct stat *stats = calloc(1, sizeof(struct stat));
@@ -156,7 +173,7 @@ void display_dir(const char *path) {
 }
 
 int nftw_callback(const char *path, const struct stat *stats, int typeflag, struct FTW *ftwbuf) {
-    (void)ftwbuf; // silence unsued variable warning
+    (void) ftwbuf; // silence unsued variable warning
     if(typeflag == FTW_F && is_time_allowed(stats)) {
         print_file(path, stats);
     }
@@ -164,6 +181,7 @@ int nftw_callback(const char *path, const struct stat *stats, int typeflag, stru
 }
 
 void display_dir_nftw(const char *path) {
+    fprintf(stderr, "NFTW Display dir %s\n", path);
     nftw(path, &nftw_callback, 512, 0);
 
 }
@@ -175,8 +193,17 @@ int main(int argc, char *argv[]) {
     }
     const char *path = argv[1];
     comparator = argv[2][0];
-    const char *date = argv[3];
-    target_time= str_to_time(date);
+
+    if(comparator != '>' && comparator != '<' && comparator != '=') {
+        fprintf(stderr, "Comparator mus be < = or >\n");
+        exit(1);
+    }
+
+//    const char *date = argv[3];
+    target_time = 0;
+    printf("target_time: %ld\n", target_time);
+    target_time = str_to_time(argv[3]);
+    printf("target_time2: %ld\n", target_time);
 
     printf("Using opendir:\n");
     display_dir(path);
