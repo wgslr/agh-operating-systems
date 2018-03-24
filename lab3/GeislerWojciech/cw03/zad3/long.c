75426d1017d91f05e06069b2a7c7f3c50a670c2d
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-03-24 01:31:22 +0100

    Create long.c simulating long cpu actions

---
+++ lab3/GeislerWojciech/cw03/zad3/long.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    double x = 253;
+    double y = -223;
+    printf("%s\n",argv[1]);
+    unsigned long operations = strtoul(argv[1], NULL, 10);
+    for(int i = 0; i < operations; ++i){
+        for(int j = 0; j < 1000; ++j){
+            x = sin(y);
+            y = sin(x);
+        }
+    }
+    printf("After %lu thousand operations result is %lf\n", operations, x);
+    return 0;
+}
+
+
+
