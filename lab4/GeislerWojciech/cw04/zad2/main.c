1bf51e899b20952cc5e4e1c60a3c2867b4279088
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-04-10 22:08:42 +0200

    Implement sendin allowances

--- lab4/GeislerWojciech/cw04/zad2/main.c
+++ lab4/GeislerWojciech/cw04/zad2/main.c
@@ -4,9 +4,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
-#include <signal.h>
 #include <stdbool.h>
 
+#define _XOPEN_SOURCE
+#include <signal.h>
+
+int N, K;
 pid_t *children;
 bool *allowed;
 int request_count = 0;
@@ -16,11 +19,11 @@ pid_t spawn_child(void);
 
 void spawn_children(int count);
 
-void request_handler(int signal) {
-    assert(signal == SIGUSR1);
+void send_allow(pid_t child);
 
-    
-}
+void allow_all(void) ;
+
+void request_handler(int signal, siginfo_t *info, void *ucontext) ;
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
@@ -28,8 +31,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    const int N = atoi(argv[1]);
-//    const int K = atoi(argv[1]);
+    sigset_t to_mask;
+    sigfillset(&to_mask);
+
+    struct sigaction *sg = calloc(1, sizeof(struct sigaction));
+    sg->sa_flags = SA_SIGINFO;
+    sg->sa_sigaction = &request_handler;
+    sg->sa_mask = to_mask;
+    sigaction(SIGTSTP, sg, NULL);
+
+    N = atoi(argv[1]);
+    K = atoi(argv[1]);
     children = calloc(N, sizeof(pid_t));
     allowed = calloc(N, sizeof(bool));
 
@@ -55,7 +67,37 @@ pid_t spawn_child(void) {
     } else {
         return pid;
     }
+}
 
-//    waitpid()
+void request_handler(int signal, siginfo_t *info, void *ucontext) {
+    assert(signal == SIGUSR1);
+    (void) ucontext; // unused
+
+    pid_t caller = info->si_pid;
+    int childnum = 0;
+    while(childnum < N && children[childnum] != caller) ++childnum;
+
+    if(!allowed[childnum]) {
+        ++request_count;
+        allowed[childnum] = true;
+    }
+
+    if(request_count == K) {
+        allow_all();
+    } else if(request_count > K) {
+        send_allow(allowed[childnum]);
+    }
+}
+
+void allow_all(void) {
+    for(int i = 0; i < N; ++i) {
+        if(allowed[i]) {
+            send_allow(children[i]);
+        }
+    }
+}
+
+void send_allow(pid_t child) {
+    kill(child, SIGALRM);
 }
 
