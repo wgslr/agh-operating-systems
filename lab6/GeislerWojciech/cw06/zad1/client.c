4b620e4cb4a1122519b5370bb25fd58b93f0a681
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-04-22 19:55:29 +0200

    6.1: Parse all commands in client

--- lab6/GeislerWojciech/cw06/zad1/client.c
+++ lab6/GeislerWojciech/cw06/zad1/client.c
@@ -29,10 +29,21 @@ int client_queue;
 int server_queue;
 
 // Splits string on whitespace
-tokens *tokenize(char *string) ;
+tokens *tokenize(char *string);
 
-void send(msgbuf *req) {
-    OK(msgsnd(server_queue, req, MSG_SZ, 0), "Error sending message");
+arith_op char_to_op(char c) ;
+
+void stop(int code) ;
+
+void send(const int mtype, const void *content, size_t content_len) {
+    msgbuf req = {
+            .sender_id = client_id, .sender_pid = getpid(),
+            .mtype = mtype,
+            .content = {0}
+    };
+    memcpy(req.content, content, content_len);
+
+    OK(msgsnd(server_queue, &req, MSG_SZ, 0), "Error sending message");
 }
 
 msgbuf *receive(void) {
@@ -42,13 +53,7 @@ msgbuf *receive(void) {
 }
 
 void register_client(void) {
-    msgbuf req = {
-            .sender_id = client_id, .sender_pid = getpid(),
-            .mtype = REGISTER,
-            .content = {0}
-    };
-    *(int *) req.content = client_queue;
-    send(&req);
+    send(REGISTER, &client_queue, sizeof(client_queue));
 
     msgbuf *resp = receive();
     client_id = *(int *) resp->content;
@@ -57,18 +62,57 @@ void register_client(void) {
 }
 
 void handle_mirror(char *message) {
-    msgbuf req = {
-            .sender_id = client_id, .sender_pid = getpid(),
-            .mtype = MIRROR,
-            .content = {0}
-    };
-    strcpy(req.content, message);
-    send(&req);
+    send(MIRROR, message, strlen(message) + 1);
 
-    printf("Mirroring. Clien id %d\n", client_id);
     msgbuf *resp = receive();
-    printf("Mirrored: '%s'\n", resp->content);
+    printf("MIRROR result: '%s'\n", resp->content);
     free(resp);
+}
+
+void handle_calc(const tokens *command) {
+    arith_req req = {
+            .op = char_to_op(command->toks[2][0]),
+            .arg1 = atoi(command->toks[1]),
+            .arg2 = atoi(command->toks[3])
+    };
+    send(CALC, &req, sizeof(req));
+
+    msgbuf *resp = receive();
+    printf("CALC result: %d\n", *(int*)resp->content);
+    free(resp);
+};
+
+void handle_time(void) {
+    send(TIME, NULL, 0);
+    msgbuf *resp = receive();
+    printf("TIME result: %s", resp->content);
+    free(resp);
+}
+
+void handle_end(void) {
+    send(END, NULL, 0);
+}
+
+arith_op char_to_op(char c) {
+    switch(c) {
+        case '+':
+            return ADD;
+        case '-':
+            return SUB;
+        case '*':
+            return MUL;
+        case '/':
+            return DIV;
+        default:
+            fprintf(stderr, "Unknown arithemetic operator\n");
+            stop(1);
+            exit(255);
+    }
+}
+
+void stop(int code) {
+    send(STOP, NULL, 0);
+    exit(code);
 }
 
 int main(int argc, char *argv[]) {
@@ -93,7 +137,7 @@ int main(int argc, char *argv[]) {
 
     char *line = NULL;
     size_t length = 0;
-    while(getline(&line, &length, input)) {
+    while(getline(&line, &length, input) > 0) {
         fprintf(stderr, "Handling line '%s'\n", line);
 
         tokens *command = tokenize(line);
@@ -103,53 +147,28 @@ int main(int argc, char *argv[]) {
             } else {
                 handle_mirror(command->toks[1]);
             }
+        } else if(strcmp(command->toks[0], "CALC") == 0) {
+            if(command->size < 4) {
+                fprintf(stderr, "You must provider <operand> <operator> <operand>");
+            } else {
+                handle_calc(command);
+            }
+        } else if(strcmp(command->toks[0], "TIME") == 0) {
+            handle_time();
+        } else if(strcmp(command->toks[0], "END") == 0) {
+            handle_end();
+        } else {
+            fprintf(stderr, "Unknown command '%s'\n", command->toks[0]);
         }
     }
     free(line);
 
+    stop(0);
 
-//    char *mirror = "Bede lustrem";
-//    to_send->mtype = MIRROR;
-//    to_send->sender_id = client_id;
-//    strcpy(to_send->content, mirror);
-//    OK(msgsnd(server_queue, to_send, MSG_SZ, 0), "Error sending mirror message");
-//
-//    read = msgrcv(client_queue, buff, MSG_SZ, 0, 0);
-//    assert(read > 0);
-//    assert(buff->sender_id == SERVER_ID);
-//    assert(buff->mtype == MIRROR_RESP);
-//
-//    printf("Mirrored: %s\n", buff->content);
-//
-//    arith_req calc_arg = {.op = MUL, .arg1 = 3, .arg2 = 4};
-//    to_send->mtype = CALC;
-//    memcpy(to_send->content, &calc_arg, sizeof(calc_arg));
-//
-//    OK(msgsnd(server_queue, to_send, MSG_SZ, 0), "Error sending calc message");
-//
-//    read = msgrcv(client_queue, buff, MSG_SZ, 0, 0);
-//    assert(read > 0);
-//    assert(buff->sender_id == SERVER_ID);
-//    assert(buff->mtype == CALC_RESP);
-//
-//    printf("Calced: %d\n", *(int *) buff->content);
-//
-//    to_send->mtype = TIME;
-//
-//    OK(msgsnd(server_queue, to_send, MSG_SZ, 0), "Error sending calc message");
-//
-//    read = msgrcv(client_queue, buff, MSG_SZ, 0, 0);
-//    assert(read > 0);
-//    assert(buff->sender_id == SERVER_ID);
-//    assert(buff->mtype == TIME_RESP);
-//
-//    printf("Time is: %s\n", buff->content);
-//
-//    to_send->mtype = END;
-
-//    OK(msgsnd(server_queue, to_send, MSG_SZ, 0), "Error sending calc message");
     return 0;
 }
+
+// Reused code from 5.1
 
 // Splits string on whitespace
 tokens *tokenize(char *string) {
