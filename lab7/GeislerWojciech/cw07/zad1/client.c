1600610681a28a8c30889c6cabda1ec6d8aa66c5
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-05-09 01:44:21 +0200

    7.1: Create client spawner

--- lab7/GeislerWojciech/cw07/zad1/client.c
+++ lab7/GeislerWojciech/cw07/zad1/client.c
@@ -2,7 +2,47 @@
 // Created by wojciech on 5/8/18.
 //
 
+#include "common.h"
+
+int semset_id;
+int shm_id;
+int repeats;
+int clients;
+
+void be_client(void) {
+    LOG("Client is born")
+    while(repeats--) {
+
+    }
+}
+
+void spawn(void){
+    pid_t pid = fork();
+    if(pid == 0) {
+        be_client();
+        exit(0);
+    } else {
+        return;
+    };
+}
+
 int main(int argc, char* argv[]) {
-    
+    if(argc != 3) {
+        fprintf(stderr, "Wrong number of arguments!\n");
+        exit(1);
+    }
+
+    clients = atoi(argv[1]);
+    repeats = atoi(argv[2]);
+
+    key_t key = get_ipc_key();
+    OK(semset_id = semget(key, SEMS, IPC_CREAT | 0600u), "Creating semaphore set failed");
+    OK(shm_id = shmget(key, sizeof(state), IPC_CREAT | 0600u), "Failed creating shared memory");
+
+
+    while(clients--) {
+        spawn();
+    }
+
     return 0;
 }
