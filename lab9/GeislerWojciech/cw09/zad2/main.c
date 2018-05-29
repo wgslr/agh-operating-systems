060f12c698bce1f7ec261791400c486de06fde85
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-05-29 22:41:07 +0200

    9.2: Implement semaphores initialization

--- lab9/GeislerWojciech/cw09/zad2/main.c
+++ lab9/GeislerWojciech/cw09/zad2/main.c
@@ -1,25 +1,34 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <bits/time.h>
+#include <time.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
 
 #define OK(_EXPR, _ERR_MSG) if((_EXPR) < -1) { fprintf(stderr, "%s: %d %s\n", _ERR_MSG, errno, strerror(errno)); exit(1); }
 
 // Print timestamped message with pid
-#define LOG(args...) { \
-    struct timespec time; \
-    clock_gettime(CLOCK_MONOTONIC, &time); \
-    char msg[256]; \
-    sprintf(msg, args); \
-    printf("%ld.%06ld %d: %s\n", time.tv_nsec, time.tv_nsec / 1000, getpid(), msg); \
-    fflush(stdout); \
+#define LOG(_VERBOSE, _PRODUCER, args...) { \
+    if(_VERBOSE || verbose) { \
+        struct timespec time; \
+        clock_gettime(CLOCK_MONOTONIC, &time); \
+        char msg[256]; \
+        sprintf(msg, args); \
+        printf("%ld.%06ld %lu(%c): %s\n", time.tv_sec, time.tv_nsec / 1000, pthread_self()%10000, _PRODUCER ? 'P' : 'C', msg); \
+        fflush(stdout); \
+    }\
 }
 
-char **buffer;
-bool verbose;
-
-sem_t **buf_sems;
-sem_t *main_sem;
+typedef struct {
+    char **buffer;
+    size_t last_write;
+    size_t last_read;
+} buffer;
 
 typedef enum {
     SMALLER = -1,
@@ -36,6 +45,43 @@ typedef struct {
     SEARCH_MODE search_mode;
     bool verbose;
 } config;
+
+
+bool verbose;
+
+buffer buff;
+
+sem_t **slot_locks;
+sem_t *queue_lock;
+sem_t *free_slots;
+
+void *produce(const config *c) {
+    LOG(1,1, "Read file %s", c->input_path);
+
+    FILE *fd = fopen(c->input_path, "r");
+
+    if(fd == NULL) {
+        fprintf(stderr, "Error opening input file for reading: %s\n", strerror(errno));
+    }
+
+    const time_t start_time = time(NULL);
+    char *line = NULL;
+    size_t len = 0;
+
+//    while(c->thread_ttl == 0 || (time(NULL) - start_time) < c->thread_ttl) {
+    while(true) {
+        // TODO end on ctrl+c
+        if(getline(&line, &len, fd) == -1) {
+            LOG(1, 1, "End of file");
+            break;
+        }
+
+//        LOG(1, 1, "Read line of length %zu", strlen(line));
+    }
+    fclose(fd);
+    free(line);
+    return NULL;
+}
 
 
 config load_config(const char *path) {
@@ -62,18 +108,39 @@ int main(int argc, char *argv[]) {
     const config c = load_config(argv[1]);
     verbose = c.verbose;
 
-    buffer = calloc(c.buffor_size, sizeof(char *));
-    buf_sems = calloc(c.buffor_size, sizeof(sem_t*));
-    main_sem = calloc(1, sizeof(sem_t));
+    buff.buffer = calloc(c.buffor_size, sizeof(char *));
+    buff.last_read = 0;
+    buff.last_write = 0;
+
+    slot_locks = calloc(c.buffor_size, sizeof(sem_t*));
+    queue_lock = calloc(1, sizeof(sem_t));
+    free_slots = calloc(1, sizeof(sem_t));
 
     for(int i = 0; i < c.buffor_size; ++i) {
-        buf_sems[i] = calloc(1, sizeof(sem_t));
-        sem_init(buf_sems[i], 0, 1);
+        slot_locks[i] = calloc(1, sizeof(sem_t));
+        sem_init(slot_locks[i], 0, 1);
     }
-    main_sem = calloc(1, sizeof(sem_t));
-    sem_init(main_sem, 0, 1);
+//    queue_lock = calloc(1, sizeof(sem_t));
+    sem_init(queue_lock, 0, 1);
+//    free_slots = calloc(1, sizeof(sem_t));
+    sem_init(free_slots, 0, c.buffor_size);
 
+    LOG(1, 0, "Main");
 
+    produce(&c);
+
+    LOG(1, 0, "Freeing");
+
+    for(int i = 0; i < c.buffor_size; ++i) {
+        sem_destroy(slot_locks[i]);
+        free(slot_locks[i]);
+    }
+    free(slot_locks);
+
+    sem_destroy(queue_lock);
+    sem_destroy(free_slots);
+    free(queue_lock);
+    free(free_slots);
 
     return 0;
 }
